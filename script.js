f8cbde76467363c3b81fc561dcba7a746a116b20
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-02-23 22:01:34 +0100

    save

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 
-function chekSpot(num, row, coll) {
-
+function used_in_row(num, row, coll){
   for (var i = 0; i < 9; i++) {
     if(i != coll){
       const obj = document.getElementById(row+','+i);
-      console.log(obj.value);
+
       if(obj.value == num){
-        console.log(false);
+        document.getElementById(row+','+coll).style.color = "red";
+        return true;
       }
     }
   }
+  return false;
+}
+
+function used_in_col(num, row, coll){
   for (var i = 0; i < 9; i++) {
     if(i != row){
       const obj = document.getElementById(i+','+coll);
-      console.log(obj.value);
+
       if(obj.value == num){
-        console.log(false);
+        document.getElementById(row+','+coll).style.color = "red";
+        return true;
       }
     }
   }
+  return false;
+}
+
+function used_in_box(num, row, coll){
+  var box_r = Math.floor(row / 3);
+  var box_c = Math.floor(coll / 3);
+
+  for (var r = box_r * 3; r < box_r * 3 + 3; r++) {
+    for (var c = box_c * 3; c < box_c * 3 + 3; c++) {
+      if(r != row && c != coll){
+        const obj = document.getElementById(r+','+c);
+
+        if(obj.value == num){
+          document.getElementById(row+','+coll).style.color = "red";
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+function chekSpot(num, row, coll) {
+
+//her h9hfjir5rf6ty7g8huj9iko0p+læ´åø mangler
+  return used_in_row
 
 }
+
 
 
 function createGamePlate() {
